3d0903794b52c515c7951e33ef41a3f0495c1043
Bhavya <bhavyashah9612@gmail.com>
2019-11-17 23:48:45 -0500

    init 6

--- app/src/main/java/com/example/assign_map/CustomInfoWindowGoogleMap.java
+++ app/src/main/java/com/example/assign_map/CustomInfoWindowGoogleMap.java
@@ -1,11 +1,8 @@
 package com.example.assign_map;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
